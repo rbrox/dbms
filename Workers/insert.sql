121be583f281f2ff56c8622bfdb59b62765291da
rbrox <rishavsci@gmail.com>
2023-09-16 05:14:51 +0530

    ques 2

--- Workers/insert.sql
+++ Workers/insert.sql
@@ -3,18 +3,24 @@ INSERT INTO WORKERS (WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEP
 VALUES
     (1, 'John', 'Doe', 50000.00, '2023-01-15', 'HR'),
     (2, 'Jane', 'Smith', 60000.00, '2023-02-20', 'IT'),
-    (3, 'Alice', 'Johnson', 55000.00, '2023-03-25', 'Sales');
+    (3, 'Alice', 'Johnson', 55000.00, '2023-03-25', 'Sales'),
+    (4, 'Michael', 'Brown', 52000.00, '2023-04-10', 'HR'),
+    (5, 'Emily', 'Davis', 58000.00, '2023-05-05', 'IT');
 
 -- Insert data into the BONUS table
 INSERT INTO BONUS (WORKER_ID, BONUS_DATE, BONUS_AMOUNT)
 VALUES
     (1, '2023-05-01', 2000.00),
     (2, '2023-05-15', 2500.00),
-    (3, '2023-06-01', 1800.00);
+    (3, '2023-06-01', 1800.00),
+    (4, '2023-06-15', 2200.00),
+    (5, '2023-07-01', 2400.00);
 
 -- Insert data into the DEPARTMENT table
 INSERT INTO DEPARTMENT (WORKER_ID, DEPT_NAME, AFFECTED_FROM)
 VALUES
     (1, 'HR', '2023-01-15'),
     (2, 'IT', '2023-02-20'),
-    (3, 'Sales', '2023-03-25');
+    (3, 'Sales', '2023-03-25'),
+    (4, 'HR', '2023-04-10'),
+    (5, 'IT', '2023-05-05'),
